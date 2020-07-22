b7a8d3b4ddbca230eda60e881ad63652bed2df13
Cristian Ortega <emanuel.ortega@hotmail.com>
2020-07-22 18:39:29 -0500

    Updated template

---
+++ PHP/test.php
@@ -0,0 +1,30 @@
+<?php
+
+    // One line comments
+
+    /*
+    Multiple
+    Lines
+    go 
+    like
+    this
+    */
+     $name = "Cristian Ortega";
+     echo $name;
+     print "Hola";
+
+     $year = 2020;
+     $born_year = 1997;
+
+     $age = $year - $born_year
+
+     if($age < 60){
+         echo "You're young"
+         echo "<br/>"  //Salto de linea
+     }
+
+     else{
+         echo "You're not young"
+     }
+
+?>
--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -8,15 +8,32 @@
     $connection = mysqli_connect($hostname, $hostuser, $hostpass, $database);
 
     if ($connection) {
-        echo "You're connected";
+        //echo "You're connected";
+        $sqlquery = "SELECT * FROM users";
+        $result = mysqli_query($connection,$sqlquery);
+        //var_dump($result);
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)){
+                //echo "Id: ".$row['id']." - Name: ".$row['name'],"</br>";
+                $user_email = $_GET['user_email'];
+                $user_password = $_GET['user_password'];
+
+                if($row['email'] == $user_email && $row['password'] == $user_password){
+                    echo "Email and user found";
+                }
+            }
+        }
+        else {
+            # code...
+        }
+        
     } else {
         echo "Sorry dude, you're NOT connected";
     }
     
 
 
-    $user_email = $_GET['user_email'];
-    $user_password = $_GET['user_password'];
+    
     
     /*
     echo $user_email;
